311e2672356916b0c37f818238ec123ceab68b99
andreasjansson <andreas@replicate.ai>
2025-05-10 13:44:12 +0200

    initial

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="toololol",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "anthropic>=0.51.0,<1"
+    ],
+)
