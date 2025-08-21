79b76d5e2889ab87832eff9551def3eba9c5d82a
andreasjansson <andreas@replicate.ai>
2025-08-21 16:42:19 -0700

    Update toololo.lib __init__.py to re-export main functions for convenience

--- toololo/lib/__init__.py
+++ toololo/lib/__init__.py
@@ -3,4 +3,18 @@
 from . import files
 from . import shell
 
-__all__ = ['files', 'shell']
+# Re-export main functions for convenience
+from .files import (
+    read_file, write_file, str_replace, delete_file, delete_files,
+    make_directory, rename_file, list_directory
+)
+from .shell import shell_command, ShellCommandResult
+
+__all__ = [
+    'files', 'shell',
+    # File functions
+    'read_file', 'write_file', 'str_replace', 'delete_file', 'delete_files',
+    'make_directory', 'rename_file', 'list_directory',
+    # Shell functions
+    'shell_command', 'ShellCommandResult'
+]
